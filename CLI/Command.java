c429d2ab61325dc25ce20a0a00fbd32de33b8813
Honda-6 <mohannad.omar06@gmail.com>
2024-10-31 20:23:20 +0300

    a5er ta3deel walahy

--- CLI/Command.java
+++ CLI/Command.java
@@ -2,12 +2,5 @@ package CLI;
 
 
 public interface Command extends Executable { //fakess abstract
-    //private String operator;
-    // private List<String> operands;
-    // Command(String op)
-    // {
-    //     //this.operator = op;
-    //     operands = new ArrayList<>();
-    // }
     public void appendOperand(String oper);
 }
